611030b3aad90ef77696d79942ffc6052dba93ef
David <ddesa@stagiaire-humanbooster.com>
2024-10-08 09:32:34 +0200

    initial commit

---
+++ src/main/java/fr/humanbooster/cda/dawid/totoenergy/TotoenergyApplication.java
@@ -0,0 +1,13 @@
+package fr.humanbooster.cda.dawid.totoenergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotoenergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotoenergyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/humanbooster/cda/dawid/totoenergy/TotoenergyApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.humanbooster.cda.dawid.totoenergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotoenergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
